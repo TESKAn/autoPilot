1ac6fd55962b7d0785e66c1b94a66ce0d47c6e87
Jure <jure@mocnik.si>
2012-12-16 10:27:40 +0100

    Moved ISR routines to stm32f4xx_it.h

--- src/stm32f4xx_it.h
+++ src/stm32f4xx_it.h
@@ -51,6 +51,19 @@ void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
 
+void ADC_IRQHandler(void);
+void TIM4_IRQHandler(void);
+void TIM8_CC_IRQHandler(void);
+void TIM8_TRG_COM_TIM14_IRQHandler(void);
+//void DMA1_Stream6_IRQHandler(void);
+void DMA1_Stream4_IRQHandler(void);
+void DMA1_Stream3_IRQHandler(void);
+//void I2C2_EV_IRQHandler(void);
+void TIM1_CC_IRQHandler(void);
+void USART1_IRQHandler(void);
+void USART2_IRQHandler(void);
+void USART3_IRQHandler(void);
+
 #ifdef __cplusplus
 }
 #endif
